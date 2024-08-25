22bf4a75d53bbf4b3e4a6940585fb68547bd63db
Kseniia <ksafonova15@gmail.com>
2024-08-25 15:10:06 +0300

    some stuff

--- main.c
+++ main.c
@@ -2,4 +2,22 @@ int main(){
 	int ad;
 	int b;
 	
+	
+	
+	qwlejkfnolwejngfowejng
+	werg;lkwnelgknwe
+	
+	
+	
+	welkgnlwejnglwe
+	
+	pwkenlpkwengpo
+	ww
+	
+	
+	welgjnwolejgnlwoejkngloweng
+	
+	
+	
+	elwnegljwnelgjknwe
 }
